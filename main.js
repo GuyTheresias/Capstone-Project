9874fb059c1bcfd7fe9301463f44da91e7a96037
Guy <gtheresias1@gmail.com>
2025-02-13 21:18:43 -0500

    Began research question page

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-console.clear();
-Splitting();
+
 
 function showInfo()
 {
     document.getElementById("question_img").hidden = true;
     document.getElementById("info").hidden = false;
 }
+
+const questionsElements = document.querySelectorAll('.questions');
+
+function checkScroll() {
+    questionsElements.forEach((element) => {
+        const elementPosition = element.getBoundingClientRect();
+        if (elementPosition.top < window.innerHeight / 2) {
+            element.classList.add('show-questions');
+        } else {
+            element.classList.remove('show-questions');
+        }
+    });
+}
+
+window.addEventListener('scroll', checkScroll);
+
