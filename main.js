75bedd67ba0b4c85fb4c70fe3a8f3fe78a769985
Guy <gtheresias1@gmail.com>
2025-03-08 01:01:34 -0500

    added 2 summary and expert page and changed nav bar formatting

--- main.js
+++ main.js
@@ -39,6 +39,83 @@ function showJobs()
     document.getElementById('jobs_a').removeAttribute('hidden')
     document.getElementById('data_a').hidden = true;
     document.getElementById('ethics_a').hidden = true;
+}
+
+function showExpertInfo(experts)
+{
+    console.log(experts);   
+    if (experts.cataleta)
+    {
+        document.getElementById('cataleta').classList.add('offset-md-3');
+        document.getElementById('cataleta_info').removeAttribute('hidden');
+        document.getElementById('brown').hidden = true;
+        document.getElementById('acemoglu').hidden = true;
+        document.getElementById('restrepo').hidden = true;
+        document.getElementById('show_experts').hidden = false;
+    }
+    if (experts.brown)
+    {
+        document.getElementById('brown').classList.add('offset-md-3');
+        document.getElementById('brown_info').removeAttribute('hidden');
+        document.getElementById('cataleta').hidden = true;
+        document.getElementById('acemoglu').hidden = true;
+        document.getElementById('restrepo').hidden = true;
+        document.getElementById('show_experts').hidden = false;
+    }
+    if (experts.acemoglu)
+    {
+        document.getElementById('acemoglu').classList.add('offset-md-3');
+        document.getElementById('acemoglu_info').removeAttribute('hidden');
+        document.getElementById('cataleta').hidden = true;
+        document.getElementById('brown').hidden = true;
+        document.getElementById('restrepo').hidden = true;
+        document.getElementById('show_experts').hidden = false;
+    }
+    if (experts.restrepo)
+    {
+        document.getElementById('restrepo').classList.add('offset-md-3');
+        document.getElementById('restrepo_info').removeAttribute('hidden');
+        document.getElementById('cataleta').hidden = true;
+        document.getElementById('brown').hidden = true;
+        document.getElementById('acemoglu').hidden = true;
+        document.getElementById('show_experts').hidden = false;
+    }
 
 }
 
+function showExperts(experts)
+{
+    if (cataleta) {
+        document.getElementById('cataleta').classList.remove('offset-md-3');
+        document.getElementById('cataleta_info').hidden = true;
+        document.getElementById('brown').hidden = false;
+        document.getElementById('acemoglu').hidden = false;
+        document.getElementById('restrepo').hidden = false;
+        document.getElementById('show_experts').hidden = true;
+    }
+    if (brown) {
+        document.getElementById('brown').classList.remove('offset-md-3');
+        document.getElementById('brown_info').hidden = true;
+        document.getElementById('cataleta').hidden = false;
+        document.getElementById('acemoglu').hidden = false;
+        document.getElementById('restrepo').hidden = false;
+        document.getElementById('show_experts').hidden = true;
+    }
+    if (acemoglu) {
+        document.getElementById('acemoglu').classList.remove('offset-md-3');
+        document.getElementById('acemoglu_info').hidden = true;
+        document.getElementById('cataleta').hidden = false;
+        document.getElementById('brown').hidden = false;
+        document.getElementById('restrepo').hidden = false;
+        document.getElementById('show_experts').hidden = true;
+    }
+    if (restrepo) {
+        document.getElementById('restrepo').classList.remove('offset-md-3');
+        document.getElementById('restrepo_info').hidden = true;
+        document.getElementById('cataleta').hidden = false;
+        document.getElementById('brown').hidden = false;
+        document.getElementById('acemoglu').hidden = false;
+        document.getElementById('show_experts').hidden = true;
+    }
+
+}
